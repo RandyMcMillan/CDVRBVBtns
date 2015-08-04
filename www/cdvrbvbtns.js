c2f8d53fa62e644e2eb50510771b7b541a181af5
Randy McMillan <randy.lee.mcmillan@gmail.com>
2015-08-04 18:45:30 -0400

    ..

--- www/cdvrbvbtns.js
+++ www/cdvrbvbtns.js
@@ -2,32 +2,32 @@
 
 var exec = require('cordova/exec');
 module.exports = {
-/*
+
 initListener: function (name, successCallback, errorCallback) {
 
    cordova.exec(successCallback, errorCallback, "CDVRBVBtns", "initListener", [name]);
 
-}
+},
 
 startListener: function (name, successCallback, errorCallback) {
 
    cordova.exec(successCallback, errorCallback, "CDVRBVBtns", "startListener", [name]);
     
-}
+},
 
 stopListener: function (name, successCallback, errorCallback) {
 
    cordova.exec(successCallback, errorCallback, "CDVRBVBtns", "stopListener", [name]);
     
-}
-*/
+},
+
 greet: function (name, successCallback, errorCallback) {
 
    cordova.exec(successCallback, errorCallback, "CDVRBVBtns", "greet", [name]);
     
 }
 
-};
+}
 
 
 
